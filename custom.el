63145e9a68d6ebdbb62411fbb5cb7cee826bcab2
Hardy5012 <410187271@qq.com>
2016-10-12 13:02:25 +0800

    update custom

--- custom.el
+++ custom.el
@@ -9,6 +9,9 @@
  '(org-agenda-files
    (quote
     ("/home/huaming_li/Dropbox/org-notes/gtd.org" "/home/huaming_li/Dropbox/org-notes/note.org")))
+ '(package-selected-packages
+   (quote
+    (org-plus-contrib youdao-dictionary yapfify yaml-mode ws-butler window-numbering which-key wgrep web-mode volatile-highlights vi-tilde-fringe uuidgen use-package toc-org tagedit stickyfunc-enhance srefactor sphinx-doc spacemacs-theme spaceline solarized-theme smex smeargle slim-mode scss-mode sass-mode restart-emacs rainbow-delimiters quelpa pyvenv pytest pyenv-mode py-isort protobuf-mode prodigy popwin pip-requirements persp-mode pcre2el paradox pangu-spacing orgit org-projectile org-present org-pomodoro org-octopress org-download org-bullets open-junk-file neotree mwim move-text monokai-theme mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum live-py-mode lispy linum-relative link-hint less-css-mode jade-mode ivy-hydra info+ indent-guide ido-vertical-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gtags helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate google-c-style golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md ggtags flyspell-correct-ivy flyspell-correct-helm flycheck-ycmd flycheck-pos-tip flx-ido find-by-pinyin-dired fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav dumb-jump disaster define-word cython-mode counsel-projectile company-ycmd company-web company-statistics company-quickhelp company-c-headers company-anaconda column-enforce-mode cmake-mode clean-aindent-mode clang-format chinese-wbim auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-pinyin ace-link ace-jump-helm-line ac-ispell)))
  '(projectile-other-file-alist
    (quote
     (("cpp" "h" "hpp" "ipp")
@@ -30,7 +33,14 @@
      ("gpg" ""))))
  '(safe-local-variable-values
    (quote
-    ((company-clang-arguments "-std=c++11" "-I/home/huaming_li/github/lytz/stockbs_svr/stockbs_svr/" "-I/home/huaming_li/rts3/src/common/ground_15_04_09/" "-I/usr/local/protobuf/include/" "-I/home/huaming_li/github/redisclient/src/")
+    ((eval setq flycheck-clang-include-path
+           (list
+            (expand-file-name "~/github/lytz/stockbs_svr/stockbs_svr/")
+            (expand-file-name "~/github/lytz/ground_15_04_09/")
+            (expand-file-name "/usr/local/protobuf/include/")
+            (expand-file-name "/home/huaming_li/github/redisclient/src/")))
+     (company-clang-arguments "-std=c++11" "-I/home/huaming_li/github/lytz/stockbs_svr/stockbs_svr/" "-I/home/huaming_li/github/lytz/ground_15_04_09/" "-I/usr/local/protobuf/include/" "-I/home/huaming_li/github/redisclient/src/")
+     (company-clang-arguments "-std=c++11" "-I/home/huaming_li/github/lytz/stockbs_svr/stockbs_svr/" "-I/home/huaming_li/rts3/src/common/ground_15_04_09/" "-I/usr/local/protobuf/include/" "-I/home/huaming_li/github/redisclient/src/")
      (eval setq flycheck-clang-include-path
            (list
             (expand-file-name "~/github/lytz/stockbs_svr/stockbs_svr/")
